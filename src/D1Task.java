dc70efa43555870373c12754fcc3e9c95330708a
RomanGitMaster <roman.yasinovskyy29@gmail.com>
2025-12-01 23:37:34 -0500

    Day 1 practice

---
+++ src/D1Task.java
@@ -0,0 +1,19 @@
+public class D1Task {
+    public static void main(String[] args) {
+        // program 1
+        System.out.println("My name is Roman, im 36 years old");
+
+        //program 2
+        int num1 = 7, num2 = 13;
+        int result = num1 + num2;
+        System.out.println("Result of adding num1 and num2 is " + result);
+
+        //program 3
+        int num3 = 11;
+        if (num3 > 10) {
+            System.out.println("More than 10");
+        } else {
+            System.out.println("Less or equal");
+        }
+    }
+}
